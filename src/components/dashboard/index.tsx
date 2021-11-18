a987e862a482fc2f51b30ea5d4c2f1ad5973a1ee
MleMoe <i@mle.moe>
2021-11-18 22:10:39 +0800

    :art: add UI wrap

--- src/components/dashboard/index.tsx
+++ src/components/dashboard/index.tsx
@@ -6,33 +6,35 @@ import styles from './index.module.scss';
 export type TimeType = 'daily' | 'weekly' | 'monthly';
 
 type DashboardProps = {
-	activities: ActivityType[];
+  activities: ActivityType[];
 };
 
 const Dashboard: FC<DashboardProps> = ({ activities }) => {
-	const timeArray: TimeType[] = ['daily', 'weekly', 'monthly'];
-	const [selectedTime, setSelectedTime] = useState<TimeType>('weekly');
+  const timeArray: TimeType[] = ['daily', 'weekly', 'monthly'];
+  const [selectedTime, setSelectedTime] = useState<TimeType>('weekly');
 
-	const onSelect = (time: TimeType) => {
-		setSelectedTime(time);
-	};
+  const onSelect = (time: TimeType) => {
+    setSelectedTime(time);
+  };
 
-	return (
-		<div className={styles.container}>
-			<div className={`${styles.item} ${styles.itemProfile}`}>
-				<ProfileCard
-					selectedTime={selectedTime}
-					timeArray={timeArray}
-					onSelect={onSelect}
-				/>
-			</div>
-			{activities.map((item, index) => (
-				<div key={index} className={styles.item}>
-					<TypeCard activity={item} timeType={selectedTime} />
-				</div>
-			))}
-		</div>
-	);
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.container}>
+        <div className={`${styles.item} ${styles.itemProfile}`}>
+          <ProfileCard
+            selectedTime={selectedTime}
+            timeArray={timeArray}
+            onSelect={onSelect}
+          />
+        </div>
+        {activities.map((item, index) => (
+          <div key={index} className={styles.item}>
+            <TypeCard activity={item} timeType={selectedTime} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export { Dashboard };
